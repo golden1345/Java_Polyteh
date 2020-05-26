61491cb95cf69586cdac37dba095d2e457e70143
golden1345 <37267119+golden1345@users.noreply.github.com>
2020-05-26 19:52:01 +0300

    Проверка

--- Kursach/Kursach/src/com/company/Main.java
+++ Kursach/Kursach/src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.io.Console;
 public class Main {
 
     public static void main(String[] args) {
-
         StudentDis studentDis = new StudentDis();
         Tests tests = new Tests();
         int n = 1;
@@ -19,10 +18,14 @@ public class Main {
         //Counting value
         n = 4;
         double alpha = 0.05;
-        System.out.println("N = "+ n + ", alpha = " + alpha + " ,Counted value: " + studentDis.dist(alpha, n));
-        System.out.println("From table value: " + tests.getVal(alpha, n));
-
-
+        double dist = studentDis.dist(alpha, n);
+        double critDist = tests.getVal(alpha, n);
+        System.out.println("N = "+ n + ", alpha = " + alpha + " ,Counted value: " + dist);
+        System.out.println("From table value: " + critDist);
+        System.out.println(
+                (dist < critDist)
+                        ? "наблюдаемый критерий < чем критический, выборка попадает под распределение стьюдента"
+                        : "наблюдаемый критерий > чем критический, выборка не попадает под распределение стьюдента");
     }
 
 
